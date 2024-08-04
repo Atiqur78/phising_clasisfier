a2e73290d3695bc232a264c760fb38a149792cb8
Atiqur78 <atikurrahman209@gmail.com>
2024-08-04 11:18:58 +0530

    update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='Atiqur Rahman',
+    author_email = 'atikurrahman209@gmail.com',
+    packages=find_packages(),
+    install_require=[],
+)
