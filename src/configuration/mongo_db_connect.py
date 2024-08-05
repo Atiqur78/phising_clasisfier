52d895b4c9f155942980d29e1560524f24c452ab
Atiqur78 <atikurrahman209@gmail.com>
2024-08-05 11:30:34 +0530

    update

---
+++ src/configuration/mongo_db_connect.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import pymongo
+import certifi
+from src.constant import *
+from src.exception import CustomException
+
+ca = certifi.where()
+
+class MongoDBClient:
+    client = None
+
+    def __init__(self, database_name = MONGO_DATABASE_NAME):
+        try:
+            if MongoDBClient.client is None:
+                mongo_db_url = os.getenv('MONGO_DB_URL')
+                if mongo_db_url is None:
+                    raise Exception('MONGO_DB_URL is not set')  
+                MongoDBClient.client = pymongo.MongoClient(mongo_db_url,tlsCAFile = ca)  
+            self.client = MongoDBClient.client
+            self.database = self.client[database_name]
+        except Exception as e:
+            raise CustomException(e, sys)
+        
---
+++ src/constant/__init__.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+import os
+AWS_S3_BUCKET_NAME = "senor_deployment"
+MONGO_DATABASE_NAME = 'phising'
+
+TARGET_COLUMN ="Result"
+
+MODEL_FILE_NAME = 'model'
+MODEL_FILE_EXTENSION = '.pkl'
+
+artifact_folder_name = datetime.now().strftime('%m_%d_%Y_%H_%M_%S')
+artifact_folder_path = os.path.join('artifacts', artifact_folder_name)
